4ff18cd89d68b775d4c4f7df30c139fc8048e9ef
dj3520 <dj3520@gmail.com>
2019-07-16 17:52:11 -0400

    Separate server specific things.
    into Server Specific Libraries

---
+++ SSLs/SA.py
@@ -0,0 +1,5 @@
+message_checks = {
+"sweetiestarr": "Unwritten rule was broken.",
+("ebola", "da wey", "uganda", "ugandan knuckles", "da wei", "de wei", "de way"): "Possible Rule 11.",
+("bipartisan", "caucus", "filibuster", "gerrymander", "politics", "republican", "democrat"): "Possible Rule 8."}
+
