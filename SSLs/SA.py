8f79e461685e3ba97328e86cbe2e096ce8462921
dj3520 <dj3520@gmail.com>
2019-10-16 14:54:39 -0400

    Update to shorter rule list.

--- SSLs/SA.py
+++ SSLs/SA.py
@@ -1,5 +1,5 @@
 message_checks = {
 "sweetiestarr": "Unwritten rule was broken.",
-("ebola", "da wey", "uganda", "ugandan knuckles", "da wei", "de wei", "de way"): "Possible Rule 11.",
-("bipartisan", "caucus", "filibuster", "gerrymander", "politics", "republican", "democrat"): "Possible Rule 8."}
-
+"discord.gg": "Possible Rule 5 violation.",
+("ebola", "da wey", "uganda", "ugandan knuckles", "da wei", "de wei", "de way"): "Possible Rule 7 violation.",
+("bipartisan", "caucus", "filibuster", "gerrymander", "politics", "republican", "democrat"): "Possible Rule 6 violation."}
